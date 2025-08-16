be9713813b84fc6055563682d240aa40825d5055
John Berean <jmberean@gmail.com>
2025-08-15 23:22:53 -0400

    index Vault ModeApplied and LiquidityAction in the subgraph

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,18 +1,28 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { Signal as SignalEvent } from "../generated/LVRShieldHook/LVRShieldHook";
-import { Signal } from "../generated/schema";
+import { ModeApplied as ModeAppliedEvent, LiquidityAction as LiquidityActionEvent } from "../generated/Vault/Vault";
+import { ModeApplied, LiquidityAction } from "../generated/schema";
 
-export function handleSignal(event: SignalEvent): void {
+export function handleModeApplied(event: ModeAppliedEvent): void {
   const id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
-  const entity = new Signal(id);
+  const e = new ModeApplied(id);
+  e.poolId = event.params.poolId;
+  e.mode = event.params.mode;
+  e.epoch = event.params.epoch;
+  e.reason = event.params.reason;
+  e.blockNumber = event.block.number;
+  e.txHash = event.transaction.hash;
+  e.save();
+}
 
-  entity.poolId = event.params.poolId;
-  entity.priceE18 = event.params.priceE18;
-  // updatedAt in the ABI is uint64, but the generated type is BigInt already.
-  entity.updatedAt = event.params.updatedAt;
-
-  entity.blockNumber = event.block.number;
-  entity.txHash = event.transaction.hash;
-
-  entity.save();
+export function handleLiquidityAction(event: LiquidityActionEvent): void {
+  const id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  const e = new LiquidityAction(id);
+  e.poolId = event.params.poolId;
+  e.mode = event.params.mode;
+  e.epoch = event.params.epoch;
+  e.baseDelta = event.params.baseDelta;
+  e.quoteDelta = event.params.quoteDelta;
+  e.reason = event.params.reason;
+  e.blockNumber = event.block.number;
+  e.txHash = event.transaction.hash;
+  e.save();
 }
